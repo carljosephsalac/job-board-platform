8ca081943bd5244c6203042e954f6ae1a0aa429b
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-07-17 11:39:08 +0800

    meet eloquent

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,6 @@ Route::get('/job/{id}', function($id) {
     $chosenJob = Job::find($id);
 
     return view('job', compact('chosenJob'));
-
-    // foreach ($jobs as $job) // using foreach
-    // {
-    //     if ($job['id'] == $id)
-    //     {
-    //         dd($job);
-    //     }
-    // }
-
-    // $job = Arr::first($jobs, function($job) use ($id) { // using use()
-    //     return $job['id'] == $id;
-    // });
-    // dd($job);
 });
 
 Route::get('/contact', function() {
