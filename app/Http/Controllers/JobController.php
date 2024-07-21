f5e2b0d31139607342005592400c0b207bd9c13f
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-07-21 22:20:18 +0800

    routes reloaded

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        // $jobs = Job::all(); // lazy loading
+        // $jobs = Job::with('employer')->paginate(); // may page number yung pagination
+        // $jobs = Job::with('employer')->cursor(); // mahaba yung url, maganda sa maramihang data
+        $jobs = Job::with('employer')->latest()->simplePaginate(5); // eager loading
+        return view('jobs.index', compact('jobs'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:3',
+            'salary' => 'required|numeric'
+        ]);
+        Job::create([
+            'employer_id' => 1,
+            'title' => $request->title,
+            'salary' => $request->salary
+        ]);
+        return redirect('/jobs')->with('created', 'Created Successfully');
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function show($id)
+    {
+        $chosenJob = Job::find($id);
+        return view('jobs.show', compact('chosenJob'));
+    }
+
+    public function edit($id)
+    {
+        $chosenJob = Job::find($id);
+        return view('jobs.edit', compact('chosenJob'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:3',
+            'salary' => 'required|numeric'
+        ]);
+        $chosenJob = Job::findOrFail($id);
+        $chosenJob->update([
+            'title' => $request->title,
+            'salary' => $request->salary
+        ]);
+        return redirect('/jobs/' . $chosenJob->id)->with('updated', 'Updated Successfully');
+    }
+    # UPDATE Route Model Binding
+    // public function update(Job $chosenJob, Request $request)
+    // {
+    //     $request->validate([
+    //         'title' => 'required|min:3',
+    //         'salary' => 'required|numeric'
+    //     ]);
+    //     $chosenJob->update([
+    //         'title' => $request->title,
+    //         'salary' => $request->salary
+    //     ]);
+    //     return redirect('/jobs/' . $chosenJob->id)->with('updated', 'Updated Successfully');
+    // }
+
+    public function destroy($id)
+    {
+        Job::findOrFail($id)->delete();
+        return redirect('/jobs')->with('deleted', 'Deleted Successfully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,86 +1,29 @@
 <?php
 
-use App\Models\Job;
-use Illuminate\Http\Request;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('home');
+Route::view('/', 'home');
+
+Route::controller(JobController::class)->group(function() {
+    Route::get('/jobs', 'index');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{id}', 'show');
+    Route::get('/jobs/{id}/edit', 'edit');
+    Route::patch('/jobs/{id}', 'update');
+    Route::delete('/jobs/{id}', 'destroy');
 });
 
-# INDEX
-Route::get('/jobs', function() {
-    // $jobs = Job::all(); // lazy loading
-    // $jobs = Job::with('employer')->paginate(); // may page number yung pagination
-    // $jobs = Job::with('employer')->cursor(); // mahaba yung url, maganda sa maramihang data
-    $jobs = Job::with('employer')->latest()->simplePaginate(5); // eager loading
-    return view('jobs.index', compact('jobs'));
-});
+// Route::resource('jobs', JobController::class);
 
-# CREATE
-Route::get('/jobs/create', function() {
-    return view('jobs.create');
-});
+// Route::resource('jobs', JobController::class, [
+//     'except' => ['show']
+// ]);
 
-# SHOW
-Route::get('/jobs/{id}', function($id) {
-    $chosenJob = Job::find($id);
-    return view('jobs.show', compact('chosenJob'));
-});
+// Route::resource('jobs', JobController::class, [
+//     'only' => ['store', 'update', 'destroy']
+// ]);
 
-# STORE
-Route::post('/jobs', function(Request $request) {
-    $request->validate([
-        'title' => 'required|min:3',
-        'salary' => 'required|numeric'
-    ]);
-    Job::create([
-        'employer_id' => 1,
-        'title' => $request->title,
-        'salary' => $request->salary
-    ]);
-    return redirect('/jobs')->with('created', 'Created Successfully');
-});
-
-# EDIT
-Route::get('/jobs/{id}/edit', function($id) {
-    $chosenJob = Job::find($id);
-    return view('jobs.edit', compact('chosenJob'));
-});
-
-# UPDATE
-Route::patch('/jobs/{id}', function($id, Request $request) {
-    $request->validate([
-        'title' => 'required|min:3',
-        'salary' => 'required|numeric'
-    ]);
-    $chosenJob = Job::findOrFail($id);
-    $chosenJob->update([
-        'title' => $request->title,
-        'salary' => $request->salary
-    ]);
-    return redirect('/jobs/' . $chosenJob->id)->with('updated', 'Updated Successfully');
-});
-# UPDATE Route Model Binding
-// Route::patch('/jobs/{chosenJob}', function(Request $request, Job $chosenJob) {
-//     $request->validate([
-//         'title' => 'required|min:3',
-//         'salary' => 'required|numeric'
-//     ]);
-//     $chosenJob->update([
-//         'title' => $request->title,
-//         'salary' => $request->salary
-//     ]);
-//     return redirect('/jobs/' . $chosenJob->id);
-// });
-
-# DELETE
-Route::delete('/jobs/{id}', function($id) {
-    Job::findOrFail($id)->delete();
-    return redirect('/jobs')->with('deleted', 'Deleted Successfully');
-});
-
-Route::get('/contact', function() {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
