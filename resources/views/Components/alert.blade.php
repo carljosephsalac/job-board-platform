242acedc00c68785ff7ee0429d61ff2b1349472a
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-07-23 20:25:57 +0800

    refactor alert and button components, and use route resource

--- resources/views/Components/alert.blade.php
+++ resources/views/Components/alert.blade.php
@@ -1,78 +1,25 @@
 @props(['color'])
 
-@if ($color === 'green')
-    <div class="flex items-center gap-4 p-4 mx-auto mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800 w-fit"
-        role="alert">
-        <span class="text-green-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-        </span>
-        <span class="sr-only">Info</span>
-        <div>
-            <span class="font-medium">{{ $slot }}</span>
-        </div>
-        <button onclick="this.parentElement.remove()" type="button"
-            class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700"
-            data-dismiss-target="#alert-1" aria-label="Close">
-            <span class="sr-only">Close</span>
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-            </svg>
-        </button>
+<div class="flex items-center gap-4 p-4 mx-auto mb-4 text-sm text-{{ $color }}-800 border border-{{ $color }}-300 rounded-lg bg-{{ $color }}-50 dark:bg-gray-800 dark:text-{{ $color }}-400 dark:border-{{ $color }}-800 w-fit"
+    role="alert">
+    <span class="text-{{ $color }}-600">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+    </span>
+    <span class="sr-only">Info</span>
+    <div>
+        <span class="font-medium">{{ $slot }}</span>
     </div>
-@elseif ($color === 'blue')
-    <div class="flex items-center gap-4 p-4 mx-auto mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800 w-fit"
-        role="alert">
-        <span class="text-blue-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-        </span>
-        <span class="sr-only">Info</span>
-        <div>
-            <span class="font-medium">{{ $slot }}</span>
-        </div>
-        <button onclick="this.parentElement.remove()" type="button"
-            class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
-            data-dismiss-target="#alert-1" aria-label="Close">
-            <span class="sr-only">Close</span>
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-            </svg>
-        </button>
-    </div>
-@elseif ($color === 'red')
-    <div class="flex items-center gap-4 p-4 mx-auto mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800 w-fit"
-        role="alert">
-        <span class="text-red-600">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-        </span>
-        <span class="sr-only">Info</span>
-        <div>
-            <span class="font-medium">{{ $slot }}</span>
-        </div>
-        <button onclick="this.parentElement.remove()" type="button"
-            class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
-            data-dismiss-target="#alert-1" aria-label="Close">
-            <span class="sr-only">Close</span>
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-            </svg>
-        </button>
-    </div>
-@endif
+    <button onclick="this.parentElement.remove()" type="button"
+        class="ms-auto -mx-1.5 -my-1.5 bg-{{ $color }}-50 text-{{ $color }}-500 rounded-lg focus:ring-2 focus:ring-{{ $color }}-400 p-1.5 hover:bg-{{ $color }}-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-{{ $color }}-400 dark:hover:bg-gray-700"
+        data-dismiss-target="#alert-1" aria-label="Close">
+        <span class="sr-only">Close</span>
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+        </svg>
+    </button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::controller(JobController::class)->group(function() {
-    Route::get('/jobs', 'index');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{id}', 'show');
-    Route::get('/jobs/{id}/edit', 'edit');
-    Route::patch('/jobs/{id}', 'update');
-    Route::delete('/jobs/{id}', 'destroy');
-});
+Route::view('/contact', 'contact');
 
-// Route::resource('jobs', JobController::class);
+Route::resource('jobs', JobController::class);
 
+// Route::controller(JobController::class)->group(function() {
+//     Route::get('/jobs', 'index');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{id}', 'show');
+//     Route::get('/jobs/{id}/edit', 'edit');
+//     Route::patch('/jobs/{id}', 'update');
+//     Route::delete('/jobs/{id}', 'destroy');
+// });
+
+# EXCEPT
 // Route::resource('jobs', JobController::class, [
 //     'except' => ['show']
 // ]);
 
+# ONLY
 // Route::resource('jobs', JobController::class, [
 //     'only' => ['store', 'update', 'destroy']
 // ]);
-
-Route::view('/contact', 'contact');
