0d7fd2b86e520c95f22ae8aba181b8af3f772b4b
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-07-29 14:42:06 +0800

    test

---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Employer;
+use App\Models\Job;
+
+it('belongs to an employer', function () {
+   // AAA
+
+   // Arrange
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id
+    ]);
+
+   // Act and Assert
+   expect($job->employer->is($employer))->toBeTrue();
+});
